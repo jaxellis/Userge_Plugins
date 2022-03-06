3bfabc127bd9d5409582ec1446f2fa596e7f71d3
jaxellis <67706810+jaxellis@users.noreply.github.com>
2022-03-06 02:07:06 -0800

    Fixes

--- plugins/tools/datetime/__main__.py
+++ plugins/tools/datetime/__main__.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 from pytz import timezone
-from .. import datetime as plugin_env
+from . import COUNTRY_CITY
 from userge import userge, Message
 
 LOG = userge.getLogger(__name__)  # logger object
 
 
-@userge.on_cmd("dt", about={
+@userge.on_cmd("datetime", about={
     'header': "Get the time and date of a City/Country/Timezone.",
     'flags': {
         '-l': "Gives list of all Country/City Combos for Heroku Config"},
@@ -29,16 +29,16 @@ async def grab_time(message: Message):
                            parse_mode="html", del_in=30)
         return
 
-    if not plugin_env.COUNTRY_CITY:
+    if not COUNTRY_CITY:
         LOG.debug("date_time: No Config Set")
         await message.edit(default_message, disable_web_page_preview=True,
                            parse_mode="html", del_in=30)
         return
 
-    datetime_now = datetime.now(timezone(plugin_env.COUNTRY_CITY))
+    datetime_now = datetime.now(timezone(COUNTRY_CITY))
     date_day_int = datetime_now.strftime('%d')
     await message.edit(" ".join(["It's", datetime_now.strftime('%I:%M %p'), "on", datetime_now.strftime('%A'), "the", date_day_int, ordinal_suffix(
-        date_day_int), "of", datetime_now.strftime('%B'), "in", plugin_env.COUNTRY_CITY.replace("_", " ")]))
+        date_day_int), "of", datetime_now.strftime('%B'), "in", COUNTRY_CITY.replace("_", " ")]))
     LOG.debug("Time: Command Finished Successfully")
 
 
